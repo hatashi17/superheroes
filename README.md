fed9e461c994e21ab4534a29eb4ee46cad6ffe4e
hatashi17 <iban.upv@gmail.com>
2022-09-23 14:29:38 +0200

    Inicio proyecto

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # superheroes
 Prueba técnica Spring boot
+
+#Inicio
+23/09/2022 14:30
---
+++ Superheroes/src/test/java/com/superheroes/gestion/SuperheroesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superheroes.gestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperheroesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
